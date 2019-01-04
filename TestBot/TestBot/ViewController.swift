c023d4a02b9c6aaf2732dd44dd48e30d2939d2fb
梁泽 <350442340@qq.com>
2019-01-04 17:57:45 +0800

    once

---
+++ TestBot/TestBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBot
+//
+//  Created by 梁泽 on 2019/1/4.
+//  Copyright © 2019 梁泽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
